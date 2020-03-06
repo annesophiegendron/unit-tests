28df01fceef613cea01fa966be2d27ae0662dc69
annesophiegendron <annesophie.gendron@gmail.com>
2020-03-06 10:53:35 +0100

    8 and 9 done

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,7 @@
 export const hackerSpeak = (words) => {
-  return words.replace(/a/gi, '4').replace(/s/gi, '5').replace(/i/gi, '1').replace(/o/gi, '0').replace(/e/gi, '3')
+
+  const string = words.replace(/a/gi, "4").replace(/e/gi, '3').replace(/i/gi, '1').replace(/o/gi, '0').replace(/s/gi,
+    '5').replace(/A/g, "4").replace(/E/g, '3').replace(/I/g, '1').replace(/O/g, '0').replace(/S/g,
+      '5')
+  return string
 }
--- src/9-hashtags.js
+++ src/9-hashtags.js
@@ -1,3 +1,5 @@
 export const hashtags = (text) => {
-  return text
+  return text.match(/#\S+/gi)
 }
+
+// https://www.ntu.edu.sg/home/ehchua/programming/howto/Regexe.html
